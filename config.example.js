48b30eaa8fab55abf18a72c67f254ddc9ffb6fc2
Espy <espimyte@gmail.com>
2026-07-09 01:37:36 -0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 export const JSON_OUTPUT_PATH = "example/achievements.json" // Where achievement data is saved
 
-export const USE_DIRECT_LINKS = true // Whether or not to use direct image links instead of downloading
+export const USE_DIRECT_LINKS = false // Whether or not to use direct image links instead of downloading
 export const ICONS_OUTPUT_FOLDER = "example/icons" // Folder where achievement icons are saved
 export const RELATIVE_IMAGE_PATH = "example" // Where stored image file paths in JSON are relative to if not using direct image links
 
